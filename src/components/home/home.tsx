be6d79cb223ef2462eae4587cc269ca2f97906dc
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-03-16 22:52:10 +0300

    fix: extend time limit check to 10 minutes and correct typo in header

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -10,7 +10,7 @@ export default function Home({student_id} : {student_id:string}) {
       <NavBar />
       <main className={`m-2 flex flex-col justify-center items-center`}>
         <div className={`w-full mt-5 sm:mt-0 p-2 sm:w-1/2 bg-base-200 rounded-lg ${IsVisible() ? '' : 'hidden'}`}>
-            <h2 className="mb-3 text-xl font-bold font-sans text-center">Todays classes</h2>
+            <h2 className="mb-3 text-xl font-bold font-sans text-center">Today's classes</h2>
             <TodaysClasses student_id={student_id}/>
         </div>
         <div className="w-full p-2 sm:h-full sm:w-1/2 mt-2 bg-base-200 rounded-lg">
